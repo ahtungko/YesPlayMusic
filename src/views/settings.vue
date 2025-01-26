5f264cdbd117191d465c8275d76d4b859743f88f
xka <itko1993@gmail.com>
2025-01-26 13:51:35 +0800

    ghg

--- src/views/settings.vue
+++ src/views/settings.vue
@@ -755,7 +755,7 @@
 
       <div class="footer">
         <p class="author"
-          >MADE BY
+        >MADE BY
           <a href="#" target="_blank">QIER222 & xkabctt</a></p
         >
         <p class="version">v{{ version }}</p>
